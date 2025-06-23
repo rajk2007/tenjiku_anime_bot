d8a8eccb453a788b8208e373d93664f61dd52c85
rajk2007 <rajkarmakar9564@gmail.com>
2025-06-23 11:25:28 +0530

    Fix config.py (add DB and channel info)

--- config.py
+++ config.py
@@ -3,3 +3,9 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 MONGO_URI = os.environ.get("MONGO_URI")
 ADMIN_IDS = [int(i) for i in os.environ.get("ADMIN_IDS", "").split(",") if i]
+
+REQUIRED_JOIN = "Crunchyroll_offical_hindi"     # your public channel username (without @)
+CHANNEL_ID = -1002570135520                     # your private channel ID
+
+DB_NAME = "tenjiku_db"
+COLLECTION_NAME = "anime"
